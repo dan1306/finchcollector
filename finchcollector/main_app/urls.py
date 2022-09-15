f316a0863108a4b8e9fb37f346807c11941f3bdf
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-09-14 23:23:34 -0400

    views and models implementation

--- finchcollector/main_app/urls.py
+++ finchcollector/main_app/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
  path('', views.home, name='home'),
  path('about/', views.about, name='about'),
  path('finch/', views.finch_index , name='finch'),
+ path('finch/<int:finch_id>/', views.finch_detail, name='finch_detail'),
+ # new route used to show a form and create a cat
+ path('finch/create/', views.FinchCreate.as_view(), name='finch_create'),
+ path('cats/<int:pk>/update/', views.FinchUpdate.as_view(), name='finch_update'),
+ path('cats/<int:pk>/delete/', views.FinchDelete.as_view(), name='finch_delete')
 ]
 
--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import Finch
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-Finch = [
-  Finch('Lolo', 'tabby', 'foul little demon', 3),
-  Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Finch('Raven', 'black tripod', '3 legged cat', 4)
-]
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Supposed to be a collector</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def finch_index(request):
-  return render(request, 'finch/index.html', { 'finch': Finch })
+  finch = Finch.objects.all()
+  return render(request, 'finch/index.html', { 'finch': finch })
+
+def finch_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finch/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = "__all__"
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url= '/finch/'
